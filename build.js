84961cc8a4903457963d06c6d2efe03e03cea550
David Neilsen <david@panmedia.co.nz>
2013-09-24 13:58:06 +1200

    Update build.js

--- build.js
+++ build.js
@@ -2,12 +2,15 @@ builder.addModule({
     name: 'Common',
     type: 'common',
     files: [
+        __dirname + '/ajax.js',
         __dirname + '/debug.js',
+        __dirname + '/event.js',
         __dirname + '/format.js',
         __dirname + '/i18n.js',
         __dirname + '/node.js',
+        __dirname + '/state.js',
         __dirname + '/template.js',
-        __dirname + '/types.js'
+        __dirname + '/types.js',
     ]
 });
 
@@ -15,6 +18,14 @@ builder.addModule({
     name: 'jQuery UI Adapter',
     type: 'common',
     files: [
-        __dirname + '/adapters/jquery-ui.js'
+        __dirname + '/adapters/jquery-ui.js',
+    ]
+});
+
+builder.addModule({
+    name: 'Pine Notify Adapter',
+    type: 'common',
+    files: [
+        __dirname + '/adapters/pnotify.js',
     ]
 });
