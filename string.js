bb474054adb723a1caeae92d0e8c3d2ddd85de83
David Neilsen <david@panmedia.co.nz>
2014-02-24 22:02:20 +1300

    Add stringFromCamelCase to string.js

--- string.js
+++ string.js
@@ -10,3 +10,9 @@ function stringHash(string) {
 function stringUcFirst(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
+
+function stringFromCamelCase(string, delimiter) {
+    return string.replace(/([A-Z])/g, function(match) {
+        return (delimiter || '-') + match.toLowerCase();
+    });
+}
