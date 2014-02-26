a21d9b886e5ceb06cbb6440f8c9007edcc8a3727
David Neilsen <david@panmedia.co.nz>
2014-02-26 13:44:49 +1300

    Add eventEventable to event.js

--- event.js
+++ event.js
@@ -13,3 +13,49 @@ function eventMouseLeave(node, callback) {
         }
     });
 };
+
+function eventEventable(object) {
+    object.prototype.events = {};
+    object.prototype.bindOptions = function(options) {
+        for (var name in options) {
+            this.bind(name, options[name]);
+        }
+    };
+    object.prototype.bind = function(name, callback) {
+        // <strict>
+        if (typeof callback === 'undefined' ||
+                !$.isFunction(callback)) {
+            handleError('Must bind a valid callback, ' + name + ' was a ' + typeof callback);
+            return;
+        }
+        // </strict>
+        var names = name.split(/,\s*/);
+        for (var i = 0, l = names.length; i < l; i++) {
+            if (!this.events[names[i]]) {
+                this.events[names[i]] = [];
+            }
+            this.events[names[i]].push(callback);
+        }
+    };
+    object.prototype.fire = function(name, args) {
+        var result = [];
+
+        // <debug>
+        if (debugLevel === MAX) {
+            debug('Firing event: ' + name);
+        }
+        // </debug>
+
+        if (this.events[name]) {
+            for (var i = 0; i < this.events[name].length; i++) {
+                var event = this.events[name][i],
+                    currentResult = event.apply(this, args);
+                if (typeof currentResult !== 'undefined') {
+                    result = result.concat(currentResult);
+                }
+            }
+        }
+
+        return result;
+    };
+};
