2d9b5fb2129841e4ce41c320a955460b937b87a7
David Neilsen <david@panmedia.co.nz>
2014-06-04 10:23:51 +1200

    Update ajax to not throw errors in IE8

--- ajax.js
+++ ajax.js
@@ -1,4 +1,11 @@
-var ajax = {};
+var ajax = function(args) {
+    var url = args.url;
+    if (args.data && args.method !== 'POST' || args.queryString) {
+        url += '?' + ajax.prepare(data);
+        args.data = undefined;
+    }
+    ajax.send(url, args.success, args.method || 'GET', args.data, args.async, args.headers || {})
+};
 
 ajax.prepare = function(data) {
     var query = [];
@@ -26,18 +33,24 @@ ajax.send = function(url, callback, method, data, async, headers) {
     x.send(data)
 };
 
-ajax.get = function(url, data, callback, async, headers) {
-    ajax.send(url + (data ? '?' + ajax.prepare(data) : ''), callback, 'GET', null, async, headers || {})
+ajax.get = function(url, data, successCallback, async, headers, method) {
+    ajax({
+        url: url,
+        data: data,
+        success: successCallback,
+        async: async,
+        headers: headers,
+        method: method
+    });
 };
 
-ajax.post = function(url, data, callback, async, headers) {
-    ajax.send(url, callback, 'POST', ajax.prepare(data), async, headers || {})
-};
-
-ajax.delete = function(url, data, callback, async, headers) {
-    ajax.send(url + (data ? '?' + ajax.prepare(data) : ''), callback, 'DELETE', null, async, headers || {})
-};
-
-ajax.put = function(url, data, callback, async, headers) {
-    ajax.send(url + (data ? '?' + ajax.prepare(data) : ''), callback, 'PUT', null, async, headers || {})
+ajax.post = function(url, data, callback, async, headers, method) {
+    ajax({
+        url: url,
+        data: data,
+        success: successCallback,
+        async: async,
+        headers: headers,
+        method: method || 'POST'
+    });
 };
